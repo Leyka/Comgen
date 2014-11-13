ae5a801c1b311f7a25dcb995069019ec39311eed
Skander <skander.kc@gmail.com>
2014-11-13 15:23:58 -0500

    Initial Commit

---
+++ src/app/Main.java
@@ -0,0 +1,28 @@
+package app;
+
+/**
+ * A small tool to useful commentary in header of each java files.
+ * I'm using it in school project to generate a static comment (like my name)
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Algorithm : 
+		 * 
+		 * Personalize the comment to add some variable (keep in textdoc and modify some parameters?) 
+		 * Pass a folder path to read java files (regex)
+		 * For each file 
+		 * 	Read each java files in this doc path
+		 * 		Keep in memory content
+		 *  	Add before content the comments
+		 * Show message of success and how much files have been modified 
+		 * -----
+		 * UI : 
+		 * Console
+		 * Swing components 
+		 */
+	}
+
+}
---
+++ src/helpers/FileHelper.java
@@ -0,0 +1,28 @@
+package helpers;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileHelper {
+
+	private final String COMMENTS_FILE_PATH = "/doc/comments.txt";
+	
+	/**
+	 * Returns true if file ends with .java
+	 * @return true or false
+	 */
+	public static boolean isJavaFile(String fileName) {
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Returns a list of java files in a recursive way 
+	 * @param folderPath folder to scan
+	 * @return list of java files
+	 */
+	public static ArrayList<File> getAllJavaFiles(String folderPath){
+		throw new UnsupportedOperationException();
+	}
+	
+	
+}

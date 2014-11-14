b7ed3c02c801ab4458d991736fdfaf11f3b59db6
Skander <skander.kc@gmail.com>
2014-11-14 13:16:26 -0500

    Add and use FileUtils lib

--- src/app/Core.java
+++ src/app/Core.java
@@ -3,7 +3,9 @@ package app;
 import helpers.FileHelper;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Core {
 	
@@ -13,13 +15,14 @@ public class Core {
 	 * Execute the operation of adding comment in the top of all java files.
 	 * @param folderPath The path of the java project (src) 
 	 * @return Count of modified java files
+	 * @throws FileNotFoundException 
 	 */
-	public int execute(String folderPath) {
+	public int execute(String folderPath) throws FileNotFoundException {
 		int modified_file_counter = 0; 
 		
 		//File comment = new File(FILE_PATH_COMMENTS); 
 		
-		ArrayList<File> javaFiles = FileHelper.getAllJavaFiles(folderPath);
+		Collection<File> javaFiles = FileHelper.getAllJavaFiles(folderPath);
 		
 		for(File file : javaFiles){
 			//FileHelper.modifyFile(file, content);
--- src/helpers/FileHelper.java
+++ src/helpers/FileHelper.java
@@ -1,35 +1,38 @@
 package helpers;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
 
-public class FileHelper {
-	/**
-	 * Returns true if file ends with .java
-	 * @return true or false
-	 */
-	public static boolean isJavaFile(String fileName) {
-		throw new UnsupportedOperationException();
-		
-		// regex
-	}
-	
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.*; 
+
+public class FileHelper {	
 	/**
 	 * Returns a list of java files in a recursive way 
 	 * @param folderPath folder to scan
 	 * @return list of java files
+	 * @throws FileNotFoundException 
 	 */
-	public static ArrayList<File> getAllJavaFiles(String folderPath){
-		throw new UnsupportedOperationException();
+	public static Collection<File> getAllJavaFiles(String folderPath) throws FileNotFoundException{
+		// file exists?
+		Path path = Paths.get(folderPath);
+		if (Files.notExists(path, LinkOption.NOFOLLOW_LINKS)){
+			throw new FileNotFoundException();
+		}
 		
-		/**
-		 * Get all files from folderpath
-		 * Foreach files 
-		 * 	Check if java file 
-		 * 		add to list of java files 
-		 * 
-		 * return the list 
-		 */
+		
+		Collection<File> files = FileUtils.listFiles(
+				new File(folderPath), 
+				new RegexFileFilter(".*\\.(java)$"), 
+				DirectoryFileFilter.DIRECTORY);
+		
+		return files; 
 	}
 	
 	public static void modifyFile(File f, String content){
